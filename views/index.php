c30cef776b89875f884a5b01858d790951359ef6
Thomas Warlop <thomas.warlop@gmail.com>
2013-06-04 14:29:49 +0200

    altered index.php to views/index.php and removed html tags and css/js tags. Now only the content of the page remains.

---
+++ views/index.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * manually include autoload to simplify installation in old sos system
+ */
+require 'vendor/autoload.php';
+
+include('bootstrap/start.php');
+// include('tests/classes.php');
+
+// $migration = new ProductLabels\Migration\Dimensions();
+// $migration->run();
+// $migration = new ProductLabels\Migration\Queue();
+// $migration->run();
+
+// $migration = new ProductLabels\Migration\Properties();
+// $migration->run();
+
+
+// $provider = new ProductLabels\ProductLabelProvider(477);
+// $products = $provider->fetchProducts();
+// var_dump($products);
+
+
+$page = isset($_GET['page']) ? $_GET['page'] : 'home';
+?>
+<div>
+	<label for="etiketCategorieSearch">
+		Bewerk Categorie
+	</label>
+	<input id='etiketCategorieSearch' type='text'>
+</div>
+
+<div id='propertyPicker'>
+	<div id="action-holder">
+		<a href="#" class="button submit-properties">Bevestigen</a>
+		<a href="#" class="button reset-properties">Ongedaan maken</a>
+		<a href="#" class="button full-reset-properties">Volledige reset</a>
+	</div>
+	<div class='left'>
+		<ul id="addedContainer"></ul>
+	</div>
+	<div class='right'>
+		<ul id="addableContainer"></ul>
+	</div>
+</div>
+
+
+
