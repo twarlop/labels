37f858e45895fdbcfdc5f5d33a96cdd8dba42750
Thomas Warlop <thomas.warlop@gmail.com>
2013-06-10 15:54:28 +0200

    fixed download link

--- presentation/index.php
+++ presentation/index.php
@@ -139,7 +139,7 @@ $page = isset($_GET['page']) ? $_GET['page'] : 'home';
 			</table>
 		</p>
 		<p>
-			<a href="#" class='downloadPdf button'>Download pdf</a>
+			<a href="/sos_tools/etiketten.php" class='button'>Download pdf</a>
 		</p>
 	</div>
 </div>
