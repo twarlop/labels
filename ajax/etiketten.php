4451ceed4536d38355ebcf601830641b35b59fd1
Thomas Warlop <thomas.warlop@gmail.com>
2013-06-11 12:15:43 +0200

    updated ajax file to include handelaars.php controller

--- ajax/etiketten.php
+++ ajax/etiketten.php
@@ -8,6 +8,38 @@ else
 	include '../vendor/twarlop/productlabels/bootstrap/start.php';
 }
 
+if(is_file('../handelaars2/controller/handelaar.php'))
+	require_once('../handelaars2/controller/handelaar.php');
+else
+{
+	
+class Setting
+{
+	protected $value;
+
+	public function __construct($value)
+	{
+		$this->value = $value;
+	}
+
+	public function setValue($value)
+	{
+		$this->$value = $value;
+	}
+
+	public function getValue()
+	{
+		return $this->value;
+	}
+}
+
+$SETTINGS = [
+	'label_type' => new Setting(1),
+	'label_taal' => new Setting(1),
+	'label_mode' => new Setting(1)
+];
+}
+
 $action = false;
 if(isset($_GET['action']))
 {
