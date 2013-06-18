65d094caaf92f8b8ee7b0d0b6aa4f413c6f4d4d7
Thomas Warlop <thomas.warlop@gmail.com>
2013-06-18 11:54:18 +0200

    label 3

---
+++ src/ProductLabels/Migration/Labels/Label3.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace ProductLabels\Migration\Labels;
+
+use ProductLabels\Contract\LabelMigrationInterface;
+use ProductLabels\Dimension\Dimension;
+use ProductLabels\Dimension\DimensionType;
+use ProductLabels\Label\Layout;
+
+
+/**
+* Label3
+*/
+class Label3 implements LabelMigrationInterface
+{
+
+	public function layout()
+	{
+		$layout = Layout::create(array(
+			'name' => '72x105 (A4) x8',
+			'landscape' => '1',
+			'is_single_label' => false,
+			'width' => '210',
+			'height' => '297',
+			'widthLabel' => '74',
+			'heightLabel' => '106',
+			'rows' => '2',
+			'columns' => '4'
+		));
+		return $layout;
+	}
+
+	public function photo()
+	{
+		$dimension = Dimension::create(array(
+			'type_id' => DimensionType::PHOTO,
+			'left' => '1',
+			'top' => '1',
+			'height' => '30',
+			'width' => '40'
+		));
+		return $dimension->id;
+	}
+
+	public function title()
+	{
+		$dimension = Dimension::create(array(
+			'type_id' => DimensionType::TITLE,
+			'left' => '1',
+			'top' => '35',
+			'height' => '5',
+			'width' => '70',
+			'font_size' => '8',
+			'bold' => '1',
+			'fill' => '0,0,0',
+			'color' => '255,255,255'
+		));
+		return $dimension->id;
+	}
+
+	public function text()
+	{
+		$dimension = Dimension::create(array(
+			'type_id' => DimensionType::TEXT,
+			'left' => '1',
+			'top' => '41',
+			'height' => '42',
+			'width' => '70',
+			'font_size' => '8',
+			'bold' => '0',
+			'max_lines' => '14',
+			'fill' => '255,255,255',
+			'color' => '0,0,0'
+		));
+		return $dimension->id;
+	}
+
+	public function promotion()
+	{
+		$dimension = Dimension::create(array(
+			'type_id' => DimensionType::PROMOTION,
+			'left' => '42',
+			'top' => '12',
+			'height' => '10',
+			'width' => '35',
+			'font_size' => '18',
+			'bold' => '1',
+			'fill' => '255,255,255',
+			'color' => '255,0,0'
+		));
+		return $dimension->id;
+	}
+
+	public function promotionText()
+	{
+		$dimension = Dimension::create(array(
+			'type_id' => DimensionType::PROMOTION_TEXT,
+			'left' => '',
+			'top' => '',
+			'height' => '',
+			'width' => '',
+			'font_size' => '',
+			'bold' => '1',
+			'fill' => '255,255,255',
+			'color' => '255,0,0'
+		));
+		return $dimension->id;
+	}
+
+	public function promotionStop()
+	{
+		$dimension = Dimension::create(array(
+			'type_id' => DimensionType::PROMOTION_STOP,
+			'left' => '45',
+			'top' => '25',
+			'height' => '5',
+			'width' => '30',
+			'font_size' => '12',
+			'bold' => '1',
+			'fill' => '255,255,255',
+			'color' => '255,0,0'
+		));
+		return $dimension->id;
+	}
+
+	public function price()
+	{
+		$dimension = Dimension::create(array(
+			'type_id' => DimensionType::PRICE,
+			'left' => '42',
+			'top' => '5',
+			'height' => '10',
+			'width' => '30',
+			'font_size' => '18',
+			'bold' => '1',
+			'fill' => '204,204,204',
+			'color' => '0,0,0'
+		));
+		return $dimension->id;
+	}
+
+	public function logoHandelaar()
+	{
+		$dimension = Dimension::create(array(
+			'type_id' => DimensionType::LOGO_HANDELAAR,
+			'left' => '2',
+			'top' => '85',
+			'height' => '20',
+			'width' => '70'
+		));
+		return $dimension->id;
+	}
+
+	public function logoMerk()
+	{
+		
+	}
+
+	
+}
