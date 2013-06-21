172f394a108af0a009d7b9cf266b75c44595db18
Thomas Warlop <thomas.warlop@gmail.com>
2013-06-21 09:13:08 +0200

    fix inclusion error?

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -1,6 +1,6 @@
 <?php
-if(is_file(__DIR__. '/../vendor/autoload.php')){
-	require __DIR__. '/../vendor/autoload.php';
+if(is_file(rtrim(__DIR__, '/'). '../vendor/autoload.php')){
+	require rtrim(__DIR__, '/'). '../vendor/autoload.php';
 }
 else if(is_file(rtrim($_SERVER['DOCUMENT_ROOT'], '/') . '/handelaars2/vendor/autoload.php')){
 	require rtrim($_SERVER['DOCUMENT_ROOT'], '/') . '/handelaars2/vendor/autoload.php';
