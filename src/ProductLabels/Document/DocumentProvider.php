1c6b7671f2a7174b7ccc29af28c9483b364b90be
Thomas Warlop <thomas.warlop@gmail.com>
2014-12-15 11:03:49 +0100

    add redirect if no layout selected
    Signed-off-by: Thomas Warlop <thomas.warlop@gmail.com>

--- src/ProductLabels/Document/DocumentProvider.php
+++ src/ProductLabels/Document/DocumentProvider.php
@@ -6,61 +6,67 @@ use ProductLabels\Pages\PageCollection;
 use TCPDF;
 
 /**
-* Document
-*
-* This is the superobject that hold all the necessary stuff to build our pdf document.
-*/
+ * Document
+ *
+ * This is the superobject that hold all the necessary stuff to build our pdf document.
+ */
 class DocumentProvider implements ProviderInterface
 {
-	/**
-	 * 
-	 * @var ProductLabels\Label\LabelProvider
-	 */
-	protected $labelProvider;
+    /**
+     *
+     * @var ProductLabels\Label\LabelProvider
+     */
+    protected $labelProvider;
 
-	/**
-	 * @var ProductLabels\Pages\PageProvider
-	 */
-	protected $pageProvider;
+    /**
+     * @var ProductLabels\Pages\PageProvider
+     */
+    protected $pageProvider;
 
-	protected $propertyProvider;
+    protected $propertyProvider;
 
-	protected $categoryProvider;
+    protected $categoryProvider;
 
-	protected $handelaar_id;
+    protected $handelaar_id;
 
-	/**
-	 * @var TCPDF;
-	 */
-	protected $pdf;
+    /**
+     * @var TCPDF;
+     */
+    protected $pdf;
 
-	public function __construct($handelaar_id, $pageProvider, $labelProvider, $propertyProvider, $categoryProvider)
-	{
-		$this->handelaar_id = $handelaar_id;
-		$this->labelProvider = $labelProvider;
-		$this->pageProvider = $pageProvider;
-		$this->propertyProvider = $propertyProvider;
-		$this->categoryProvider = $categoryProvider;
-	}
+    public function __construct($handelaar_id, $pageProvider, $labelProvider, $propertyProvider, $categoryProvider)
+    {
+        $this->handelaar_id = $handelaar_id;
+        $this->labelProvider = $labelProvider;
+        $this->pageProvider = $pageProvider;
+        $this->propertyProvider = $propertyProvider;
+        $this->categoryProvider = $categoryProvider;
+    }
 
-	protected function layout()
-	{
-		return $this->labelProvider->fetchLayout();
-	}
+    protected function layout()
+    {
+        $layout = $this->labelProvider->fetchLayout();
 
-	public function createDocument($products)
-	{
-		$layout = $this->layout();
-		if($layout->landscape)
-		{
-			$pdf = new TCPDF('L', 'mm', array($layout->width, $layout->height));
-		}
-		else
-		{
-			$pdf = new TCPDF('P', 'mm', array($layout->width, $layout->height));
-		}
-		$collection = $this->pageProvider->collection($layout->itemsPerPage(), $products);
-		return new Document($pdf, $layout, $collection, $this->propertyProvider, $this->categoryProvider, $this->labelProvider);
-	}
-	
+        if (!$layout) {
+            $referer = $_SERVER['HTTP_REFERER'];
+
+            header('Location:' . $referer);
+            exit();
+        }
+        return $layout;
+    }
+
+    public function createDocument($products)
+    {
+        $layout = $this->layout();
+        if ($layout->landscape) {
+            $pdf = new TCPDF('L', 'mm', array($layout->width, $layout->height));
+        } else {
+            $pdf = new TCPDF('P', 'mm', array($layout->width, $layout->height));
+        }
+
+        $collection = $this->pageProvider->collection($layout->itemsPerPage(), $products);
+        return new Document($pdf, $layout, $collection, $this->propertyProvider, $this->categoryProvider, $this->labelProvider);
+    }
+
 }
